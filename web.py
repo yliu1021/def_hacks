1395133da43729be51860c144b9e5dc734ec1732
Vonais <kevinho688@hotmail.com>
2016-04-17 00:44:52 -0400

    asdf

--- web.py
+++ web.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request, flash, redirect
 from werkzeug import secure_filename
+import os
+
+UPLOAD_FOLDER = 'static'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/' , methods = ['GET','POST'])
 def home():
 	if request.method == 'POST':
 		f = request.files['file']
-		f.save(secure_filename(f.filename))
+		filename = secure_filename(f.filename)
+		print(filename)
+		f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 	return render_template('index.html')
    
 if __name__ == '__main__':
