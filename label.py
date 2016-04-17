73a329edf57b18878f5a94ee3ebae3f21732301a
Yuhan Liu <Yuhan@Yuhans-MacBook-Pro.local>
2016-04-16 20:40:18 -0400

    adfa

--- label.py
+++ label.py
@@ -5,42 +5,64 @@ import httplib2
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-# The url template to retrieve the discovery document for trusted testers.
 api = 'AIzaSyCzLFH3ZdvRoaLSXsp7fSK-NkHBG8kwEUM'
 apiVersion = "v1"
 DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
 
+AvailFeatures = {
+    'label':      "LABEL_DETECTION",
+    'text':       "TEXT_DETECTION",
+    'face':       "FACE_DETECTION",
+    'landmark':   "LANDMARK_DETECTION",
+    'logo':       "LOGO_DETECTION",
+    'safeSearch': "SAFE_SEARCH_DETECTION",
+    'imageProperties':      "IMAGE_PROPERTIES"
+}
+    
+class ImageRecognition():
+    
+    def __init__(self, imagePath):
+        with open(imagePath) as image:
+            self.imageData = base64.b64encode(image.read()).decode("UTF-8")
+            
+        credentials = GoogleCredentials.get_application_default()
+        self.service = discovery.build('vision', 'v1', credentials=credentials,
+                                  discoveryServiceUrl=DISCOVERY_URL)
+    
+    
+    def getResponse(self, features):
+        b = self._createRequest(features)
+        request = self.service.images().annotate(body=b)
+        response = request.execute()
+        return self._parseResponse(response)
+        
+    
+    def _parseResponse(self, responses):
+        response['responses']
+        for response in responses:
+            if response == 'labelannotation':
+                response[]
+                
+        return response
+        
+        
+    def _createRequest(self, features):
+        image = {'content': self.imageData}
+
+        f = []
+        for feat in features:
+            f.append({
+                'type': feat,
+                'maxResults': 5
+            })
+        
+        r = [{'image': image, 'features': f}]
+        b = {'requests': r}
+        return b
+    
 #key AIzaSyCzLFH3ZdvRoaLSXsp7fSK-NkHBG8kwEUM
 
-def detect_face(face_file, max_results=4):
-    """Uses the Vision API to detect faces in the given file.
-
-    Args:
-        face_file: A file-like object containing an image with faces.
-
-    Returns:
-        An array of dicts with information about the faces in the picture.
-    """
-    image_content = face_file.read()
-    batch_request = [{
-        'image': {
-            'content': base64.b64encode(image_content).decode('UTF-8')
-            },
-        'features': [{
-            'type': 'FACE_DETECTION',
-            'maxResults': max_results,
-            }]
-        }]
-
-    service = get_vision_service()
-    request = service.images().annotate(body={
-        'requests': batch_request,
-        })
-    response = request.execute()
-
-    return response['responses'][0]['faceAnnotations']
-
-def main(photo_file):
+def getResults(photo_file):
     """Run a label request on a single image"""
 
     credentials = GoogleCredentials.get_application_default()
@@ -56,19 +78,28 @@ def main(photo_file):
                 },
                 'features': [{
                     'type': 'LABEL_DETECTION',
-                    'maxResults': 1
+                    'maxResults': 5
+                }, {
+                    'type': 'LANDMARK_DETECTION',
+                    'maxResults': 5
+                }, {
+                    'type': 'IMAGE_PROPERTIES',
+                    'maxResults': 5
                 }]
             }]
         })
         response = service_request.execute()
-        label = response['responses'][0]['labelAnnotations'][0]['description']
-        print('Found label: %s for %s' % (label, photo_file))
-        return 0
+        for i in response['responses'][0]['imagePropertiesAnnotation']['dominantColors']['colors']:
+            print(i)
+        return map(lambda x: (x['description'], x['score']), response['responses'][0]['labelAnnotations'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('image_file', help='The image you\'d like to label.')
     args = parser.parse_args()
-    # main(args.image_file)
-    with open(args.image_file) as image:
-        print(detect_face(image))
+    # print(getResults(args.image_file))
+    i = ImageRecognition(args.image_file)
+    print(i.getResponse([AvailFeatures['label']]))
+    
+    #fskdfhdsiufhdskfhdkshf
+    
